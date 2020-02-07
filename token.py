d54947b0c98a777fb259d8ab83335c256621cd0f
ges4688 <gonzalosalvia@gmail.com>
2020-02-07 16:43:11 -0300

    Update token.py

--- token.py
+++ token.py
@@ -1,6 +1,6 @@
 #!py
 
-# The following statefile uses Python to set a grain ('sample-token') to
+# The following statefile uses Python to set a grain ('mitoken') to
 # a value pulled from a HTTP call. The call will not be repeated if the
 # grain is already set.
 
@@ -12,17 +12,17 @@ def run():
     return {
       'token': {
         'test.configurable_test_state': [
-          {'name': 'sample-token'},
+          {'name': 'mitoken'},
           {'result': True},
-          {'changes': not grains.get('sample-token')}
+          {'changes': not grains.get('mitoken')}
         ]
       }
     }
   return {
    'token': {
      'grains.present': [
-        {'name': 'sample-token'},
-        {'value': grains.get('sample-token') or json.loads(
+        {'name': 'mitoken'},
+        {'value': grains.get('mitoken') or json.loads(
           query('http://jsonplaceholder.typicode.com/posts/1',
                 decode_type=False)['body'])['id']}
       ]
